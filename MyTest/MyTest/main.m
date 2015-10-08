6544062652814a28ec8ebfb095531d612c30ec67
sw517227087 <sky@skydeMacBook-Pro.local>
2015-10-08 11:54:38 +0800

    创建项目

---
+++ MyTest/MyTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTest
+//
+//  Created by sky on 15/10/8.
+//  Copyright © 2015年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
